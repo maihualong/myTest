cdff3694f1894d4ef94ecda4a3b40b0674c681a6
maihualong <1901918881@qq.com>
2022-09-04 14:59:57 +0800

    1

--- DayByExercise/src/September/Test1.java
+++ DayByExercise/src/September/Test1.java
@@ -6,5 +6,10 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("234535");
 
+
+    }
+
+    private static void show(){
+        System.out.println("show");
     }
 }
