a9ee1cc7e2d78118de2a4aca0b61df4d1f34b60e
maihualong <1901918881@qq.com>
2022-09-04 13:37:46 +0800

    1

--- DayByExercise/src/September/Test1.java
+++ DayByExercise/src/September/Test1.java
@@ -1,7 +1,11 @@
 package September;
 
+import java.util.Scanner;
+
 public class Test1 {
     public static void main(String[] args) {
+        System.out.println("234535");
+        Scanner sc = new Scanner(System.in);
 
     }
 }
